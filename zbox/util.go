1e94670aaa761e19f2e741067e9fd3d5c7471a17
Krishna <kupadhyaya@techracers.io>
2020-03-04 15:02:40 +0530

    Add Encrypt-Decrypt functions

--- zbox/util.go
+++ zbox/util.go
@@ -1,7 +1,32 @@
 package zbox
 
-import "github.com/0chain/gosdk/zboxcore/sdk"
+import (
+	"encoding/hex"
+
+	"github.com/0chain/gosdk/zboxcore/sdk"
+	"github.com/0chain/gosdk/zboxcore/zboxutil"
+)
 
 func GetClientEncryptedPublicKey() (string, error) {
 	return sdk.GetClientEncryptedPublicKey()
+}
+
+func Encrypt(key, text string) (string, error) {
+	keyBytes := []byte(key)
+	textBytes := []byte(text)
+	response, err := zboxutil.Encrypt(keyBytes, textBytes)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(response), nil
+}
+
+func Decrypt(key, text string) (string, error) {
+	keyBytes := []byte(key)
+	textBytes, _ := hex.DecodeString(text)
+	response, err := zboxutil.Decrypt(keyBytes, textBytes)
+	if err != nil {
+		return "", err
+	}
+	return string(response), nil
 }
